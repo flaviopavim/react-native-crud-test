ce44ba6a02e83943dc8eda6486494104c2b006ac
Flávio Pavim <contato@flaviopavim.com.br>
2022-01-31 11:57:07 -0400

    updates

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -9,6 +9,25 @@ const db = DatabaseConnection.getConnection();
 const HomeScreen = ({ navigation }) => {
   let [flatListItems, setFlatListItems] = useState([]);
 
+  useEffect(() => {
+    db.transaction(function (txn) {
+      txn.executeSql(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='table_user'",
+        [],
+        function (tx, res) {
+          console.log('item:', res.rows.length);
+          if (res.rows.length == 0) {
+            txn.executeSql('DROP TABLE IF EXISTS table_user', []);
+            txn.executeSql(
+              'CREATE TABLE IF NOT EXISTS table_user(user_id INTEGER PRIMARY KEY AUTOINCREMENT, user_name VARCHAR(20), user_contact INT(10), user_address VARCHAR(255))',
+              []
+            );
+          }
+        }
+      );
+    });
+  }, []);
+
   //useEffect(() => {
     db.transaction((tx) => {
       tx.executeSql(
