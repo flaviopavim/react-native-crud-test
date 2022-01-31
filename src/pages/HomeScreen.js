410cdc470362a6e2474aad7b2e442e41b45c1f95
Flávio Pavim <contato@flaviopavim.com.br>
2022-01-31 11:59:40 -0400

    updates

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -12,14 +12,19 @@ const HomeScreen = ({ navigation }) => {
   useEffect(() => {
     db.transaction(function (txn) {
       txn.executeSql(
-        "SELECT name FROM sqlite_master WHERE type='table' AND name='table_user'",
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='table_note'",
         [],
         function (tx, res) {
           console.log('item:', res.rows.length);
           if (res.rows.length == 0) {
-            txn.executeSql('DROP TABLE IF EXISTS table_user', []);
+            txn.executeSql('DROP TABLE IF EXISTS table_note', []);
             txn.executeSql(
-              'CREATE TABLE IF NOT EXISTS table_user(user_id INTEGER PRIMARY KEY AUTOINCREMENT, user_name VARCHAR(20), user_contact INT(10), user_address VARCHAR(255))',
+              'CREATE TABLE IF NOT EXISTS table_note ('+
+                'note_id INTEGER PRIMARY KEY AUTOINCREMENT, '+
+                'note_title VARCHAR(20), '+
+                'note_date VARCHAR(20), '+
+                'note_content VARCHAR(255) '+
+              ')',
               []
             );
           }
